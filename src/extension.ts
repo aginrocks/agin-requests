b8a7d6f9bdb1586fd87aba7d161616a20441c2c3
Tymek Vonau <vonautymek@gmail.com>
2025-01-16 00:49:34 +0100

    [core] added aliases (NOT WORKING)

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import 'module-alias/register';
 import * as vscode from 'vscode';
 import { SidebarProvider } from './SidebarProvider';
 import createRequestWebview from './createRequestView';
